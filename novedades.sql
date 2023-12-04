2bd6dae70b74f2fe5081139cf9f3ea49e91665d4
Jorge Rodriguez <dalerojo2000@gmail.com>
2023-12-04 19:27:01 -0300

    avances

--- novedades.sql
+++ novedades.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 04-12-2023 a las 02:28:30
+-- Tiempo de generación: 04-12-2023 a las 22:23:41
 -- Versión del servidor: 8.0.31
 -- Versión de PHP: 8.0.26
 
@@ -42,10 +42,10 @@ CREATE TABLE IF NOT EXISTS `novedades` (
 --
 
 INSERT INTO `novedades` (`id`, `titulo`, `subtitulo`, `cuerpo`, `img_id`) VALUES
-(1, 'NUEVOS CENTROS LOGISTICOS EN ZONA NORTE Y PARQUE INDUSTRIAL', 'EXPANDIMOS NUESTRA RED DE DEPOSITOS EN ZONA NORTE GBA', 'Incorporamos a nuestra de red 5 nuevos depósitos con infraestructura de ultima generacion. \r\nsistema de rackeados pushback y drive-in.\r\nAutoelevadores contrabalanceados y retráctiles Eléctricos. \r\n\r\n                        ', 'cttxsvplbrtbhzs3w2h0'),
-(2, 'EQUIPOS TESLA DE ULTIMA GENERACION', 'EN RUTAS DEL SOL CUIDAMOS EL MEDIO AMBIENTE INCLINANDONOS HACIA LA TRANSFORMACION.', 'En 2023 incorporamos a nuestra flota de camiones equipos TESLA de ultima generación, porque en Logistica & distribución Rutas del Sol S.R.L. no solo nos ocupamos de tu empresa, sino que también tenemos un gran compromiso en el cuidado del planeta.  \r\n                        ', 'ubmowc4qgd3l7drm7pva'),
-(24, 'Nuevos camiones', 'camiones electricos Tesla', 'equipos de Vanguardia', 'wsal4syvq3o5l8cequux'),
-(38, 'INAUGURAMOS CENTRO DE DISTRIBUCION', '10000 posiciones de racks ', 'La solucion logistica que su empresa necesita', 'jrswsgcu5unceavef1rz');
+(1, 'NUEVOS CENTROS LOGISTICOS EN ZONA NORTE Y PARQUE INDUSTRIAL', 'EXPANDIMOS NUESTRA RED DE DEPOSITOS EN ZONA NORTE GBA', 'Incorporamos a nuestra red 5 nuevos depósitos con infraestructura de ultima generación. \r\nsistema de rackeados pushback y drive-in.\r\nAutoelevadores contrabalanceados y retráctiles Eléctricos. \r\n\r\n                        \r\n                        \r\n                        ', 'qyar543xftiazmogoyrd'),
+(2, 'EQUIPOS TESLA DE ULTIMA GENERACION', 'EN RUTAS DEL SOL CUIDAMOS EL MEDIO AMBIENTE INCLINANDONOS HACIA LA TRANSFORMACION.', 'En 2023 incorporamos a nuestra flota de camiones equipos TESLA de ultima generación, porque en Logistica & distribución Rutas del Sol S.R.L. no solo nos ocupamos de tu empresa, sino que también tenemos un gran compromiso en el cuidado del planeta.  \r\n                        \r\n                        \r\n                        ', 'xu4eycqce2bsixl6kgtr'),
+(24, 'CAMIONES CISTERNAS PARA TRANSPORTE DE INFLAMABLES', 'CISTERNAS CON BOMBAS Y COMPARTIMIENTOS', 'Incluimos en nuestra flota 5 equipos cisternas para transportar líquidos de grado alimenticio        \r\n\r\n                        \r\n                        ', 'sc43e8mq7wkgoakkvlfu'),
+(38, 'TE GUSTARIA SER PARTE DE RUTAS DEL SOL?', 'Estamos en búsqueda de personal para sumar a nuestro equipo de unidad transporte. ', 'Buscamos técnicos mecánicos e ingenieros de sistemas. \r\nSi estas interesado en postularte a los puestos vacantes, envía tu CV a  nuestro equipo de RRHH. \r\n                        \r\n                        \r\n                        \r\n                        ', 'p44apjgoob0punztuxqx');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
