11b15c6f8d77cf61601df6f1d5c25fa66d81ede0
Jorge Rodriguez <dalerojo2000@gmail.com>
2023-12-03 23:29:45 -0300

    avances

---
+++ novedades.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 04-12-2023 a las 02:28:30
+-- Versión del servidor: 8.0.31
+-- Versión de PHP: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto_final_logistica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `novedades`
+--
+
+DROP TABLE IF EXISTS `novedades`;
+CREATE TABLE IF NOT EXISTS `novedades` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(250) NOT NULL,
+  `subtitulo` text NOT NULL,
+  `cuerpo` text NOT NULL,
+  `img_id` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Volcado de datos para la tabla `novedades`
+--
+
+INSERT INTO `novedades` (`id`, `titulo`, `subtitulo`, `cuerpo`, `img_id`) VALUES
+(1, 'NUEVOS CENTROS LOGISTICOS EN ZONA NORTE Y PARQUE INDUSTRIAL', 'EXPANDIMOS NUESTRA RED DE DEPOSITOS EN ZONA NORTE GBA', 'Incorporamos a nuestra de red 5 nuevos depósitos con infraestructura de ultima generacion. \r\nsistema de rackeados pushback y drive-in.\r\nAutoelevadores contrabalanceados y retráctiles Eléctricos. \r\n\r\n                        ', 'cttxsvplbrtbhzs3w2h0'),
+(2, 'EQUIPOS TESLA DE ULTIMA GENERACION', 'EN RUTAS DEL SOL CUIDAMOS EL MEDIO AMBIENTE INCLINANDONOS HACIA LA TRANSFORMACION.', 'En 2023 incorporamos a nuestra flota de camiones equipos TESLA de ultima generación, porque en Logistica & distribución Rutas del Sol S.R.L. no solo nos ocupamos de tu empresa, sino que también tenemos un gran compromiso en el cuidado del planeta.  \r\n                        ', 'ubmowc4qgd3l7drm7pva'),
+(24, 'Nuevos camiones', 'camiones electricos Tesla', 'equipos de Vanguardia', 'wsal4syvq3o5l8cequux'),
+(38, 'INAUGURAMOS CENTRO DE DISTRIBUCION', '10000 posiciones de racks ', 'La solucion logistica que su empresa necesita', 'jrswsgcu5unceavef1rz');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
